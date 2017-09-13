07edbbff865a9d3a02ca4cd85b52b9d0a05dffe0
garrettpen98 <gpenfield@dmacc.edu>
2017-09-12 19:19:31 -0500

    Inital Class Creation

---
+++ foodItem.cpp
@@ -0,0 +1,13 @@
+Class foodItem {
+string name, type, notes;
+double price;
+
+private:
+	void setName();
+	void setType();
+	void setnotes();
+public:
+	string getName();
+	string getType();
+	string getnotes();
+}
---
+++ restaurantClass.cpp
@@ -0,0 +1,15 @@
+Class Restaurant {
+	
+string restaurantName;
+string serverName;
+int capacity;
+
+private:
+	void restaurantName();
+	void setServerName();
+	void setCapacity();
+public:
+	string getRestaurantName();
+	string getServerName();
+	int getCapacity();
+}
