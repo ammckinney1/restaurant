9fc5374855813ed9af2d4adcb9b0d03a9a3c977d
garrettpen98 <gpenfield@dmacc.edu>
2017-09-19 18:37:51 -0500

    Defined getters and setters
    Defined getters and setters for foodItem, and restaurantClass.

--- foodItem.cpp
+++ foodItem.cpp
@@ -11,4 +11,28 @@ public:
 	string getName() const;
 	string getType() const;
 	string getnotes() const;
-}
+};
+
+void setName(string _name){
+	name = _name;
+};
+
+void setType(string _type){
+	type = _type;
+};
+
+void setnotes(string _note){
+	note = _note;
+};
+
+string getName(){
+	return name;
+};
+
+string getType(){
+	return type;
+};
+
+string getnotes(){
+	return notes;
+};
--- restaurantClass.cpp
+++ restaurantClass.cpp
@@ -11,4 +11,28 @@ public:
 	string getRestaurantName() const;
 	string getServerName() const;
 	int getCapacity() const;
-}
+};
+
+void setRestaurantName(string _restaurantName){
+	restaurantName = _restaurantName;
+};
+
+void setServerName(string _serverName){
+	serverName = _serverName;
+};
+
+void setCapacity(int _capacity){
+	capacity = _capacity;
+};
+
+string getRestaurantName(){
+	return restaurantName
+};
+
+string getServerName(){
+	return serverName;
+};
+
+int getCapacity(){
+	return capacity;
+};
